6ba3e1fab9a7fd1ccf73a1e46b7aaaaff2e70756
xlinkfrostx <71806327+xlinkfrostx@users.noreply.github.com>
2021-01-24 19:42:06 -0500

    Moving files around

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const webapp = express();
 webapp.use("/static", express.static(path.resolve(__dirname, "src", "static")));
 
 webapp.get("/*", (request, resolve) => {
-    resolve.sendFile(path.resolve(__dirname, "src", "index.html"));
+    resolve.sendFile(path.resolve(__dirname, "./", "index.html"));
 });
 
 webapp.listen(process.env.PORT || 49152, () => console.log("Website is running"));
